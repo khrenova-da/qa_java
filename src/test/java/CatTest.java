78b863d749d789f2381d8432b43742418d50705c
Darya Khrenova <khrenova-da@yandex.ru>
2023-02-25 22:43:36 +0300

    first version

---
+++ src/test/java/CatTest.java
@@ -0,0 +1,32 @@
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CatTest {
+    @Mock
+    Feline feline;
+
+    @Test
+    public void getSoundReturnsMeow() {
+        Cat cat = new Cat(feline);
+        String actual = cat.getSound();
+        Assert.assertEquals("Мяу", actual);
+    }
+
+    @Test
+    public void getFoodReturnsListOfFood() throws Exception {
+        Cat cat = new Cat(feline);
+        Mockito.when(feline.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        List<String> actual = cat.getFood();
+        List<String> expected = List.of("Животные", "Птицы", "Рыба");
+        Assert.assertEquals(expected, actual);
+    }
+}
